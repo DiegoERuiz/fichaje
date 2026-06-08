844d871af7ae7f4d39d563415ab2e9d325328e27
DESKTOP-J64J33H\usuario <it@biedma.com>
2026-06-08 10:36:34 +0200

    Fix bugs

--- apps/fichaje-be/src/main/java/org/fichaje/dto/AuthResponse.java
+++ apps/fichaje-be/src/main/java/org/fichaje/dto/AuthResponse.java
@@ -2,6 +2,7 @@ package org.fichaje.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.Builder.Default;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -13,12 +14,15 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class AuthResponse {
-    
+
     private String accessToken;
     private String refreshToken;
+
+    @Default
     private String tokenType = "Bearer";
+
     private Long expiresIn; // Segundos
-    
+
     public AuthResponse(String accessToken, String refreshToken) {
         this.accessToken = accessToken;
         this.refreshToken = refreshToken;
